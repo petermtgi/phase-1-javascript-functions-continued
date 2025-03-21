629071159a7aa5502f01f74b328ca676defa8df2
Peter Mutungi <3sjk0krrr@gmail.com>
2025-03-21 09:32:57 +0300

    js-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// Function 1: saturdayFun - Default activity for Saturdays
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function 2: mondayWork - Default activity for Mondays
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function 3: wrapAdjective - Wraps an adjective with given flair
+  function wrapAdjective(flair = "*") {
+    return function (adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Export functions for testing
+  module.exports = { saturdayFun, mondayWork, wrapAdjective };
+  
